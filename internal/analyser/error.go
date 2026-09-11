85adab5c71ea033ffa1153f495ef0127e20c77c0
Gui97p <guipassaroto07@gmail.com>
2026-09-11 20:31:39 -0300

    helper methods to produce errors

---
+++ internal/analyser/error.go
@@ -0,0 +1,11 @@
+package analyser
+
+import "fmt"
+
+func (a *Analyser) error(e string) {
+	a.errors = append(a.errors, e)
+}
+
+func (a *Analyser) errorf(e string, args ...any) {
+	a.error(fmt.Sprintf(e, args))
+}
