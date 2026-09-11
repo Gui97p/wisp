61827b6a85c7136e3b0dbd265b37ec68a00540f0
Gui97p <guipassaroto07@gmail.com>
2026-09-11 20:31:22 -0300

    implemented Type interface correctly for structs with String() and Equals() methods

--- internal/analyser/type.go
+++ internal/analyser/type.go
@@ -1,5 +1,10 @@
 package analyser
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Type interface {
 	String() string
 	Equals(Type) bool
@@ -9,13 +14,49 @@ type PrimitiveType struct {
 	Name string
 }
 
+func (p PrimitiveType) String() string {
+	return p.Name
+}
+
+func (p PrimitiveType) Equals(other Type) bool {
+	o, ok := other.(PrimitiveType)
+	if !ok {
+		return false
+	}
+	return p.Name == o.Name
+}
+
 type PointerType struct {
 	Element Type
+}
+
+func (p PointerType) String() string {
+	return "*" + p.Element.String()
+}
+
+func (p PointerType) Equals(other Type) bool {
+	o, ok := other.(PointerType)
+	if !ok {
+		return false
+	}
+	return p.Element.Equals(o.Element)
 }
 
 type ArrayType struct {
 	Element Type
 	Size    int64
+}
+
+func (a ArrayType) String() string {
+	return fmt.Sprintf("%s[%d]", a.Element.String(), a.Size)
+}
+
+func (a ArrayType) Equals(other Type) bool {
+	o, ok := other.(ArrayType)
+	if !ok {
+		return false
+	}
+	return a.Size == o.Size && a.Element.Equals(o.Element)
 }
 
 type StructType struct {
@@ -24,7 +65,87 @@ type StructType struct {
 	Order  []string
 }
 
+func (s *StructType) String() string {
+	var b strings.Builder
+	b.WriteString("struct ")
+	b.WriteString(s.Name)
+	b.WriteString(" { ")
+	for _, field := range s.Order {
+		tp := s.Fields[field]
+		b.WriteString(tp.String())
+		b.WriteRune(' ')
+		b.WriteString(field)
+		b.WriteRune(' ')
+	}
+	b.WriteRune('}')
+
+	return b.String()
+}
+
+func (s *StructType) Equals(other Type) bool {
+	o, ok := other.(*StructType)
+	if !ok {
+		return false
+	}
+	return s.Name == o.Name
+}
+
 type FuncType struct {
+	Name    string
 	Params  []Type
 	Returns []Type
+}
+
+func (f *FuncType) String() string {
+	var b strings.Builder
+	b.WriteString("func ")
+	b.WriteString(f.Name)
+	b.WriteRune('(')
+	for i, tp := range f.Params {
+		b.WriteString(tp.String())
+		if i != len(f.Params)-1 {
+			b.WriteString(", ")
+		}
+	}
+	b.WriteRune(')')
+
+	b.WriteString(" (")
+	for i, tp := range f.Returns {
+		b.WriteString(tp.String())
+		if i != len(f.Returns)-1 {
+			b.WriteString(", ")
+		}
+	}
+	b.WriteRune(')')
+
+	return b.String()
+}
+
+func (f *FuncType) Equals(other Type) bool {
+	o, ok := other.(*FuncType)
+	if !ok {
+		return false
+	}
+	if len(f.Params) != len(o.Params) || len(f.Returns) != len(o.Returns) {
+		return false
+	}
+	for i := range f.Params {
+		if !f.Params[i].Equals(o.Params[i]) {
+			return false
+		}
+	}
+	for i := range f.Returns {
+		if !f.Returns[i].Equals(o.Returns[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+var primitives = map[string]bool{
+	"int": true, "int8": true, "int16": true, "int32": true, "int64": true,
+	"uint": true, "uint8": true, "uint16": true, "uint32": true, "uint64": true,
+	"float32": true, "float64": true,
+	"char": true, "string": true,
+	"bool": true,
 }
