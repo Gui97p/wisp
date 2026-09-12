381805be09a76c6e63373167b89decf3b31ae155
Gui97p <guipassaroto07@gmail.com>
2026-09-11 23:49:45 -0300

    var stmt check

--- internal/analyser/stmt.go
+++ internal/analyser/stmt.go
@@ -20,15 +20,21 @@ func (a *Analyser) checkStmt(stmt ast.Statement) {
 		}
 	case *ast.ReturnStmt:
 		a.checkReturnStmt(s)
-	// case *ast.VarStmt:
-	// case *ast.IfStmt:
-	// case *ast.ForStmt:
-	// case *ast.LoopStmt:
-	// case *ast.AssignStmt:
-	// case *ast.IncDecStmt:
-	// case *ast.BreakStmt, *ast.ContinueStmt:
+	case *ast.VarStmt:
+		a.checkVarStmt(s)
 	case *ast.MultiVarStmt:
 		a.checkMultiVarStmt(s)
+	// case *ast.AssignStmt:
+	// a.checkAssignStmt(s)
+	// case *ast.IncDecStmt:
+	// a.checkIncDecStmt(s)
+	// case *ast.IfStmt:
+	// a.checkIfStmt(s)
+	// case *ast.ForStmt:
+	// a.checkForStmt(s)
+	// case *ast.LoopStmt:
+	// a.checkLoopStmt(s)
+	// case *ast.BreakStmt, *ast.ContinueStmt:
 	case *ast.BlockStmt:
 		a.enterScope()
 		a.checkBlock(s)
@@ -73,5 +79,44 @@ func (a *Analyser) checkMultiVarStmt(stmt *ast.MultiVarStmt) {
 		if !a.scope.Define(sym) {
 			a.errorf("variable already declared: %s", name)
 		}
+	}
+}
+
+func (a *Analyser) checkVarStmt(stmt *ast.VarStmt) {
+	var declaredType Type
+	hasExplicitType := stmt.Type.Name != ""
+
+	if hasExplicitType {
+		declaredType = a.resolveTypeRef(a.scope, stmt.Type)
+	}
+
+	var valueType Type
+	if stmt.Value != nil {
+		valueType = a.checkExpr(stmt.Value)
+	}
+
+	var finalType Type
+	switch {
+	case hasExplicitType && stmt.Value == nil:
+		finalType = declaredType
+	case hasExplicitType && stmt.Value != nil:
+		if declaredType != nil {
+			if _, invalid := valueType.(InvalidType); !invalid && !valueType.Equals(declaredType) {
+				a.errorf("variable %s declared as %s, got %s", stmt.Name, declaredType.String(), valueType.String())
+			}
+		}
+		finalType = declaredType
+	default:
+		a.errorf("variable %s doesn't have a type or value", stmt.Name)
+		finalType = InvalidType{}
+	}
+
+	if finalType == nil {
+		finalType = InvalidType{}
+	}
+
+	symbol := &Symbol{Name: stmt.Name, Kind: VAR, Type: finalType}
+	if !a.scope.Define(symbol) {
+		a.errorf("variable %s already declared in this scope", stmt.Name)
 	}
 }
