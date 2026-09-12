b6885aabf958164ef4138db6af0067343bef2d55
Gui97p <guipassaroto07@gmail.com>
2026-09-11 23:26:25 -0300

    multi var stmt check

--- internal/analyser/stmt.go
+++ internal/analyser/stmt.go
@@ -25,11 +25,47 @@ func (a *Analyser) checkStmt(stmt ast.Statement) {
 	// case *ast.LoopStmt:
 	// case *ast.AssignStmt:
 	// case *ast.IncDecStmt:
-	// case *ast.MultiVarStmt:
 	// case *ast.BreakStmt, *ast.ContinueStmt:
+	case *ast.MultiVarStmt:
+		a.checkMultiVarStmt(s)
 	case *ast.BlockStmt:
 		a.enterScope()
 		a.checkBlock(s)
 		a.exitScope()
+	}
+}
+
+func (a *Analyser) checkMultiVarStmt(stmt *ast.MultiVarStmt) {
+	var types []Type
+
+	if len(stmt.Values) == 1 {
+		if call, ok := stmt.Values[0].(*ast.CallExpr); ok {
+			types = a.checkCallExpr(call)
+		} else {
+			types = []Type{a.checkExpr(stmt.Values[0])}
+		}
+	} else {
+		types = make([]Type, len(stmt.Values))
+		for i, v := range stmt.Values {
+			types[i] = a.checkExpr(v)
+		}
+	}
+
+	if len(types) != len(stmt.Names) {
+		a.errorf("expected %d values, got %d", len(stmt.Names), len(types))
+	}
+
+	for i, name := range stmt.Names {
+		var t Type
+		if i < len(types) {
+			t = types[i]
+		} else {
+			t = InvalidType{}
+		}
+
+		sym := &Symbol{Name: name, Kind: VAR, Type: t}
+		if !a.scope.Define(sym) {
+			a.errorf("variable already declared: %s", name)
+		}
 	}
 }
