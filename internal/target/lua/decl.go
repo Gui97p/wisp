764d7a39b80d6d78278c11cc5e04b66086a6cf1e
Gui97p <guipassaroto07@gmail.com>
2026-09-15 19:25:52 -0300

    function params transpiling

--- internal/target/lua/decl.go
+++ internal/target/lua/decl.go
@@ -8,7 +8,14 @@ import (
 )
 
 func compileFunc(b *strings.Builder, fd *ast.FuncDecl) error {
-	fmt.Fprintf(b, "local function %s()\n", fd.Name)
+	fmt.Fprintf(b, "local function %s(", fd.Name)
+	for k, v := range fd.Params {
+		b.WriteString(v.Name)
+		if k != len(fd.Params)-1 {
+			b.WriteByte(',')
+		}
+	}
+	b.WriteString(")\n")
 
 	for _, stmt := range fd.Body.Statements {
 		if err := compileStatement(b, stmt); err != nil {
