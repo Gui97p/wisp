3ed7d2d116ba89b5cfc63727d9f3502df9ceab4a
Gui97p <guipassaroto07@gmail.com>
2026-09-16 17:32:16 -0300

    cobra CLI package & root setup

---
+++ cmd/main/root.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:          "wisp",
+	Short:        "Wisp compiler toolchain",
+	SilenceUsage: true,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(buildCmd, luaCmd, debugCmd)
+}
