1b98d73962a7818f07bd26face0d3ecb751b0cad
Gui97p <guipassaroto07@gmail.com>
2026-09-17 10:30:05 -0300

    type declaration parsing

--- internal/parser/decl.go
+++ internal/parser/decl.go
@@ -19,38 +19,31 @@ func (p *Parser) parseDeclaration() ast.Declaration {
 }
 
 func (p *Parser) parseDeclarationInner() ast.Declaration {
+	var decl ast.Declaration
+
 	switch p.current.Type {
 	case lexer.TOKEN_CONST:
-		decl := p.parseConstDeclaration()
-		if decl == nil {
-			return nil
-		}
-		return decl
+		decl = p.parseConstDeclaration()
 	case lexer.TOKEN_FUNC:
-		decl := p.parseFuncDeclaration()
-		if decl == nil {
-			return nil
-		}
-		return decl
+		decl = p.parseFuncDeclaration()
 	case lexer.TOKEN_STRUCT:
-		decl := p.parseStructDeclaration()
-		if decl == nil {
-			return nil
-		}
-		return decl
+		decl = p.parseStructDeclaration()
+	case lexer.TOKEN_TYPE:
+		decl = p.parseTypeDeclaration()
 	case lexer.TOKEN_IMPORT:
-		decl := p.parseImportDeclaration()
-		if decl == nil {
-			return nil
-		}
-		return decl
+		decl = p.parseImportDeclaration()
 	case lexer.TOKEN_EXPORT:
-		return p.parseExportDeclaration()
+		decl = p.parseExportDeclaration()
 	default:
 		p.errorf("expected declaration, got %s", p.current.Type.DisplayName())
 		p.advance()
 		return nil
 	}
+
+	if decl == nil {
+		return nil
+	}
+	return decl
 }
 
 func (p *Parser) parseImportDeclaration() *ast.ImportDecl {
@@ -105,6 +98,14 @@ func (p *Parser) parseExportDeclaration() ast.Declaration {
 		decl.Exported = true
 		return decl
 
+	case lexer.TOKEN_TYPE:
+		decl := p.parseTypeDeclaration()
+		if decl == nil {
+			return nil
+		}
+		decl.Exported = true
+		return decl
+
 	default:
 		p.error("expected declaration after export")
 		return nil
@@ -131,4 +132,29 @@ func lastPathSegment(path string) string {
 		return path
 	}
 	return path[idx+1:]
+}
+
+func (p *Parser) parseTypeDeclaration() *ast.TypeDecl {
+	decl := &ast.TypeDecl{}
+
+	if !p.expect(lexer.TOKEN_IDENT) {
+		return nil
+	}
+	decl.Name = p.current.Literal
+
+	p.advance()
+	ref := p.parseTypeDefinitionPrefix()
+	if ref == nil {
+		return nil
+	}
+	if !p.parseArraySuffix(ref) {
+		return nil
+	}
+	decl.Underlying = *ref
+
+	if !p.expect(lexer.TOKEN_SEMICOLON) {
+		return nil
+	}
+
+	return decl
 }
